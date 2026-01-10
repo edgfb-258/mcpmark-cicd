867aca90bd4eaf1694ebe8b3d4e49d0b3efdf250
edgfb-258 <y1tww07j@171307.xyz>
2026-01-11 05:15:49 +0800

    Fix linting errors in src/example.js

--- src/example.js
+++ src/example.js
@@ -1,6 +1,1 @@
-const unusedVar = "this is unused";
-console.log("This uses double quotes and no semicolon")
-function test() {
-  let x = 5
-  return x
-}
+function test() {\n  let x = 5;\n  return x;\n}
